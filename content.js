99f2a13f51d556fdfe1ceba486c36d7149644d6d
Paul Gowder <paul.gowder@gmail.com>
2016-10-24 22:57:07 -0500

    iteration 1 fails with table.GetElementsByTagName is not a function

--- content.js
+++ content.js
@@ -1,3 +1,26 @@
 // extract data from everything in table form
+function ARRAYDAMNIT(someArrayLikeObject) {
+    return [].slice.call(someArrayLikeObject);
+}
 
-tables = document.getElementsByTagName("table")
+var tables = ARRAYDAMNIT(document.getElementsByTagName("table"));
+var tabulardata = tables.map(extractTable);
+
+function extractTable(table) {
+    let rows = ARRAYDAMNIT(table.GetElementsByTagName("tr"));
+    let headers = ARRAYDAMNIT(table.GetElementsByTagName("th"));
+    return {"headers": headers, "rows": rows.map(extractRows)};
+}
+
+function extractRows(row) {
+    let cells = ARRAYDAMNIT(row.GetElementsByTagName(""));
+    return cells.map(extractContent);
+}
+
+function extractContent(cell) {
+    return cell.innerText;
+}
+
+// a bit of testing
+var dasdiv = document.createElement('div');
+dasdiv.innerHTML = JSON.stringify(tabulardata);
