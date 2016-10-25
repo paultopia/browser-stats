c491c6107eb785498131c78fd9e08501aa7c3da2
Paul Gowder <paul.gowder@gmail.com>
2016-10-25 13:42:28 -0500

    simple fix to arrays

--- statspop/src/statspop/core.cljs
+++ statspop/src/statspop/core.cljs
@@ -2,10 +2,12 @@
     (:require [reagent.core :as reagent]))
 
 
-(def test-mini (js/jStat (array [[1 2] [3 4] [5 6]])))
+(def test-mini (js/jStat (clj->js [[1 2] [3 4] [5 6]])))
 
 (defn test-jstat [arr]
   (str (js/jStat.dimensions (js/jStat arr))))
+
+;; might just be worth rolling my own with github.com/thinktopic/aljabr for the matrix math.  except actually neither it nor thi-ng/ndarray seem to want to provide actual matrix math... there is also more js, in toji/gl-matrix. 
 
 (.log js/console test-mini)
 (.log js/console (.dimensions test-mini))
