ed4943288efc5efb9a0a6ec974fdd4e584187576
Paul Gowder <paul.gowder@gmail.com>
2016-10-26 21:50:52 -0500

    failed attempt at passing object constructor in 1

--- statspop/src/statspop/core.cljs
+++ statspop/src/statspop/core.cljs
@@ -17,18 +17,20 @@
 
 (defn show-line-chart
   [chart-data]
-  (let [{:keys [data options]} chart-data]
+  (let [{:keys [func data options]} chart-data]
     (.log js/console (str "showing line chart with: " data))
-    (js/Chartist.Line. ".ct-chart" (clj->js data) (clj->js options))))
+    (func ".ct-chart" (clj->js data) (clj->js options))))
 
 
-(def test-line-data1 {:data {:labels ["Mar-2012" "Jun-2012" "Nov-2012" "Oct-2013" "Nov-2014"]
+(def test-line-data1 {:func js/Chartist.Line.
+                      :data {:labels ["Mar-2012" "Jun-2012" "Nov-2012" "Oct-2013" "Nov-2014"]
                              :series [[1 1 6 15 25]]}
                       :options {:width  "700px"
                                 :height "380px"}
                        :class "ct-chart ct-perfect-fourth"})
 
-(def test-line-data2 {:data {:labels ["Mar-2013" "Jun-2013" "Nov-2013" "Oct-2014" "Nov-2015"]
+(def test-line-data2 {:func js/Chartist.Line.
+                      :data {:labels ["Mar-2013" "Jun-2013" "Nov-2013" "Oct-2014" "Nov-2015"]
                              :series [[25 15 6 1 1]]}
                       :options {:width  "700px"
                                 :height "380px"}
