60cad4a8ce13f7ab40b8aa29b2bf71e2381ae024
Paul Gowder <paul.gowder@gmail.com>
2016-10-26 21:54:19 -0500

    successful passing function in using new constructor rather than dot
    http://www.spacjer.com/blog/2014/09/12/clojurescript-javascript-interop/

--- statspop/src/statspop/core.cljs
+++ statspop/src/statspop/core.cljs
@@ -19,17 +19,17 @@
   [chart-data]
   (let [{:keys [func data options]} chart-data]
     (.log js/console (str "showing line chart with: " data))
-    (func ".ct-chart" (clj->js data) (clj->js options))))
+    (new func ".ct-chart" (clj->js data) (clj->js options))))
 
 
-(def test-line-data1 {:func js/Chartist.Line.
+(def test-line-data1 {:func js/Chartist.Line
                       :data {:labels ["Mar-2012" "Jun-2012" "Nov-2012" "Oct-2013" "Nov-2014"]
                              :series [[1 1 6 15 25]]}
                       :options {:width  "700px"
                                 :height "380px"}
                        :class "ct-chart ct-perfect-fourth"})
 
-(def test-line-data2 {:func js/Chartist.Line.
+(def test-line-data2 {:func js/Chartist.Line
                       :data {:labels ["Mar-2013" "Jun-2013" "Nov-2013" "Oct-2014" "Nov-2015"]
                              :series [[25 15 6 1 1]]}
                       :options {:width  "700px"
