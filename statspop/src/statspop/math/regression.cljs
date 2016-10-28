12625dce79f451179fd9c4267ebb6f021d45a60b
Paul Gowder <paul.gowder@gmail.com>
2016-10-28 11:16:42 -0500

    implement linear regression

--- statspop/src/statspop/math/regression.cljs
+++ statspop/src/statspop/math/regression.cljs
@@ -15,19 +15,28 @@
 (defn linear-regression
   "estimate OLS parameters.
 
-  vector, nested numeric vectors --> vector"
+  nested numeric vectors, nested numeric vectors --> vector
+
+  expects rowwise input, e.g. labels are [[lab-row-1] [lab-row-2]] and
+  features are [[feat-1-row-1 feat-2-row-1] [feat-1-row-2] [feat-2-row-2]]
+
+  to transform column-wise data in native clj datastructures, use local helper function
+  cols->rows below."
   [labels features]
   (let [x (matrix/matrixify (add-intercept features))
-        y (matrix/matrixify [labels])
+        y (matrix/matrixify labels)
         x-transpose (matrix/transpose x)
         xtx-1 (matrix/invert (matrix/multiply x-transpose x))
         xty (matrix/multiply x-transpose y)]
     (matrix/m-to-cljs (matrix/multiply xtx-1 xty))))
 
+(defn cols->rows
+  "assumes you have a single vector of labels [l1 l2 l3]
+  and nested vectors of features [[f1-row1 f1-row2 f1-row3] [f2-row1 f2-row2 f2-row3]]
 
-(def test-data
-  {:labels [9.95 24.45 31.75 35.00 25.02 16.86 14.38 9.60 24.35 27.50 17.08 37.00 41.95 11.66 
-            21.65 17.89 69.00 10.30 34.93 46.59 44.88 54.12 56.63 22.13 21.15]
-   :features [[2 8 11 10 8 4 2 2 9 8 4 11 12 2 4 4 20 1 10 15 15 16 17 6 5]
-              [50 110 120 550 295 200 375 52 100 300 412 400 500 360 205 400 600
-               585 540 250 290 510 590 100 400]]})
+  Just nests the labels in another vector and then transposes them both. "
+  [labels features]
+  {:labels (apply mapv vector [labels])
+   :features (apply mapv vector features)})
+
+
