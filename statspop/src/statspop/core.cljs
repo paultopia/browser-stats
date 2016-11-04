06b1602ae9267501e86a9457f94620d581a358c4
Paul Gowder <paul.gowder@gmail.com>
2016-11-04 11:56:37 -0500

    try out some more stats stuff

--- statspop/src/statspop/core.cljs
+++ statspop/src/statspop/core.cljs
@@ -16,10 +16,23 @@
   (str (js/jStat.dimensions (js/jStat arr))))
 
 (defn test-jstat-distri-api []
-  (str "calling cdf function: "
-       (js/jStat.chisquare.cdf 14.067 7) ; test statistic -> p-value
-       " calling inverse function: "
-       (js/jStat.chisquare.inv .95 7))) ; p-value -> test-statistic
+  (str "chi-square cdf function (test-stat->p - should be .05): "
+       (js/jStat.chisquare.cdf 14.067 7)
+       " | chi-square inverse function (p->test-stat - should be 14.067): "
+       (js/jStat.chisquare.inv .95 7)
+       " | ONE-TAILED t-test-square cdf function (test-stat->p): "
+       (js/jStat.studentt.cdf 1.895 7) 
+       " | ONE-TAILED t-test-square inverse function (p->test-stat): "
+       (js/jStat.studentt.inv .95 7)
+       " | STANDARDIZED ONE-TAILED z-test(normal)-square cdf function (test-stat->p): "
+       (js/jStat.normal.cdf 1.645 0 1) 
+       " | STANDARDIZED ONE-TAILED z-test(normal)-square inverse function (p->test-stat): "
+       (js/jStat.normal.inv .95 0 1)
+       ))
+
+;; these above are all correct.  for p-values, I'm working with percentiles so need to subtract these percentiles from 1.  For two-tailed t-test, I should just double the p value.  Don't really need to provide p-value -> test statistic functions, should just provide test-stat to p-value functions. For z-scores, this is all standardized.  parameters for chi square and t are test statistic and degrees of freedom; parameters for normal are test statistic, mean, and sd.
+
+;; should also add f-distro for anova. it's all in jStat.centralF and parameters are test stat, df1, and df2. 
 
 (defcard-rg test-card
   "testing devcards"
